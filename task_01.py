b73c827f9c8bc55a60c46156abf96ed8f1200d28
maxim <maxim.ryklis@spsmail.cuny.edu>
2016-10-12 13:00:11 -0400

    Update task_01.py

--- task_01.py
+++ task_01.py
@@ -1,4 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module demonstrates the fibonacci sequence."""
+
+
 def fibonacci(maxint):
+	"""Function produces fibonacci sequence up to the maxint
+	
+	Args:
+	    maxint (int): number to set limit for sequence
+	    
+	Returns:
+	    fib_list (list): returns fibonacci sequence of numbers
+	    
+	Example:
+	    >>>fibonacci(10)
+	    [0, 1, 1, 2, 3, 5, 8]
+	"""
+	
 	a, b = 0, 1
 	fib_list = [a]
 	while b < maxint:
