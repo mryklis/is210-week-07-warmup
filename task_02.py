e26e02d02ffdcbf43092407118b870f05787a3ee
maxim <maxim.ryklis@spsmail.cuny.edu>
2016-10-12 13:04:50 -0400

    Update task_02.py

--- task_02.py
+++ task_02.py
@@ -1,4 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module evaluates truthiness."""
+
+
 def bool_to_str(bval):
+	"""Function compares boolean values and returns string
+	
+	Args:
+	    bval (bool): either has text or empty
+	    
+	Returns:
+	    strval (str): returns 'Yes' or 'No'
+	    
+	Example:
+	    >>> import task_02
+	    >>> bool_to_str(True)
+	    'Yes'
+
+	    >>> import task_02
+	    >>> bool_to_str('')
+	    'No'
+	"""
 	if bval:
 		strval = 'Yes'
 	else:
