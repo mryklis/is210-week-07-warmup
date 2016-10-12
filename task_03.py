2d177cd5ba9a4b36b7eb535f0c110f4ce0277c8a
maxim <maxim.ryklis@spsmail.cuny.edu>
2016-10-12 13:08:02 -0400

    Update task_03.py

--- task_03.py
+++ task_03.py
@@ -1,6 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module produces the max, min, and avg words per line."""
+
+
 import decimal
 
 def lexicographics(to_analyze):
+	"""Function produces fibonacci sequence up to the maxint
+	
+	Args:
+	    to_analyze (str): input string to be analyzed
+	    
+	Returns:
+	    tup (tuple): tuple containing 2 integers and a decimal.
+	    
+	Example:
+	   >>> lexicographics('''Don't stop believing,
+	   Hold on to that feeling.''')
+	   (5, 3, Decimal(4.0))
+	"""
 	count_words = []
 	for char in to_analyze.split('\n'):
 		length = len(char.split(' '))
